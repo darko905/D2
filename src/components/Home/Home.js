27401997e3c9a36034ddcb1973d00e9c18764c7f
Darko <djukicdarko58@gmail.com>
2023-02-05 13:12:40 +0100

    created structure

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='home section' id='home'>
+            <div className='home__container container '>
+                <div className='home__content '>
+                
+                    <div className='home__data'>
+                        <h1 className='home__title'>D2 Design</h1>
+                        <h3 className='home__subtitle'>Mi Vam pruzamo sigurnost, kvalitet, poverljivost</h3>
+                
+                        <button className='button button-flex'>Kontakt</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+  )
+}
+
+export default Home

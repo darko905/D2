9b23b8287889ea8a92b0457fdf46b0b113bd1848
Darko <djukicdarko58@gmail.com>
2023-02-26 15:42:51 +0100

    Create Service components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Service from './components/Service/Service';
 import './styles/App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <main className='main'>
       <Home/>
       <About/>
+      <Service/>
     </main>
     </>
   );
---
+++ src/components/Service/Service.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { BiCodeBlock, BiMobile } from "react-icons/bi";
+import { TbRoad, TbBrandGoogleAnalytics } from "react-icons/tb";
+import { MdBrush } from "react-icons/md";
+import { RiComputerLine, RiSettings5Line } from "react-icons/ri";
+
+const Service = () => {
+  return (
+    <section className='section service' id='service'>
+            <h2 className='section__title'>Naše usluge</h2>
+            <span className='section__subtitle'>MI NUDIMO REŠENJA PROBLEMA, A NE SAMO USLUGU</span>
+
+            <div className='service__container container grid'>
+                <div className='service__content'>
+                    <div>
+                        <RiComputerLine className='service__icons'/>
+                        <h3 className='service__title'>IZRADA SOFTVERA ZA RAD SA BAZAMA PODATAKA</h3>
+                    </div>
+                    <p className='service__descritpion'>Izrada različitih vrsta softwer-a za rad sa bazama podataka koje će poboljšati efikasnost vašeg poslovanja.</p>
+                </div>
+                <div className='service__content'>
+                    <div>
+                        <BiCodeBlock className='service__icons'/>
+                        <h3 className='service__title'>IZRADA WEB SAJTOVA</h3>
+                    </div>
+                    <p className='service__descritpion'>Napravićemo profesionalnu web stranicu, sa modernim dizajnom, koja će predstavljati Vaš biznis na najbolji mogući način.</p>
+                </div>
+                <div className='service__content'>
+                    <div>
+                        <RiSettings5Line className='service__icons'/>
+                        <h3 className='service__title'>REDIZAJN WEB SAJTA</h3>
+                    </div>
+                    <p className='service__descritpion'>Ako je vašem sajtu potrebno osvežavanje, prilagođavanje za mobilne uređaje ili reorganizacija sadržaja, na pravom ste mestu.</p>
+                </div>
+                <div className='service__content'>
+                    <div>
+                        <BiMobile className='service__icons'/>
+                        <h3 className='service__title'>RESPONSIVE DIZAJN</h3>
+                    </div>
+                    <p className='service__descritpion'>Ukoliko želite da dizajn Vašeg web sajta izgleda podjednako dobro na svim uređajima, onda je responsive web sajt nešto o čemu morate da razmišljate.</p>
+                </div>
+                <div className='service__content'>
+                    <div>
+                        <MdBrush className='service__icons'/>
+                        <h3 className='service__title'>GRAFIČKI DIZAJN</h3>
+                    </div>
+                    <p className='service__descritpion'>Logo dizajn, manipulacija slika, baneri, flajeri, dizajn odeće,korice za knjige, jelovnici…</p>
+                </div>
+                <div className='service__content'>
+                    <div>
+                        <TbBrandGoogleAnalytics className='service__icons'/>
+                        <h3 className='service__title'>SEO INTERNET PREZENTACIJA</h3>
+                    </div>
+                    <p className='service__descritpion'>Optimizacija za pretraživače (SEO) je skup radnji koji omogućava da se vaš sajt što bolje rangira.</p>
+                </div>
+            </div>
+        </section>
+  )
+}
+
+export default Service

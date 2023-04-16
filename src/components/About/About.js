aa6ac30b200bdbbccc4fec8d21edd5eff64b3e9e
Darko <djukicdarko58@gmail.com>
2023-04-16 08:57:47 +0200

    Modified about component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,24 +4,35 @@ import AboutImg from '../../assests/aboutImg.webp';
 const About = () => {
   return (
     <section className='section about' id='about'>
-            <h2 className='section__title'>O NAMA</h2>
+            <h2 className='section__title'>ZAŠTO BAŠ MI?</h2>
             <span className='section__subtitle'>D2 DESIGN</span>
             <div className='about__container container grid'>
                 
                 <img src={AboutImg} alt='About_image' className="about__img"/>
                 <div className='about__data'>
-                    <h3 className='about__title'>PROFESIONALNO MENJAMO DIGITALNI PEJZAŽ SVETA, FIRMU PO FIRMU.</h3>
+                    <h3 className='about__title'>KREATIVNOST</h3>
+                    <p className='about__subtitle'>Uvek smo u potrazi za novim izazovima. Oni nas inspirišu.</p>
                     <p className='about__descritpion'>
-                    Agencija za digitalni marketing sa sedištem u Užicu. Koja se bavi pružanjem širokog spektra usluga iz oblasti digitalnog marketinga. Naš tim čine iskusni dizajneri i programeri koji će učiniti sve da vaše ideje pretvore u realnost.
-                    Sve zahteve klijenata shvatamo ozbiljno i trudimo se da svakim rešenjem prevaziđemo očekivanja i učinimo da naše rešenje pozitivno utiče na vaš biznis.
-                    Primarni cilj je da svaki budući klijent postane deo zajednice gde ćemo zajedno graditi i unapređivati naše poslovanje u svim pravcima.
+                    Sve naše ideje stoje vam na raspolaganju! Vašem biznisu potrebna je naša kreativnost, a našoj kreativnost potreban je vaš biznis.
                     </p>
-                    <a href='#contact'>
+
+                    <h3 className='about__title'>ISKUSTVO</h3>
+                    <p className='about__subtitle'>Ne postojimo od juče. Tu smo već 5 godina.</p>
+                    <p className='about__descritpion'>
+                    Naše iskustvo sa klijentima iy najrazličitijih sfera poslovanja, biće vam od značajne koristi pri odabiiru poslovne strategiie idealne za vaš brend. 
+                    </p>
+
+                    <h3 className='about__title'>TRANSPARENSTNOST</h3>
+                    <p className='about__subtitle'>Najviše vrednujemo transparentan rad. Kao i otvoren, prijateljski odnos.</p>
+                    <p className='about__descritpion'>
+                    U radu sa nama imaćete pristum svim vašim nalozima za oglašavanje, i kompletnu kontrolu nas raspodelom budzeta i njegovom potrošnjom.
+                    </p>
+                    {/* <a href='#contact'>
                         <button className='button button--flex'>
-                            {/* Kontakt <MdPhoneEnabled/> */}
+                            
                             Kontakt
                         </button>
-                    </a>
+                    </a> */}
                 </div>
                 
                 
